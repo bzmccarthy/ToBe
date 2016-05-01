494dbed590e33c102a96cdb8a964ff021db07461
bzmccarthy <bzmccarthy@gmail.com>
2016-05-01 17:26:48 -0400

    v0.00001

--- ToBe.py
+++ ToBe.py
@@ -3,14 +3,29 @@ Real Lives."""
 
 def main():
 
-    """Main function initiate and run game"""
+    """Main function to initiate and run game"""
 
-    print("Welcome to Thrive. Enjoy your stay.")
-    input("Press ENTER to continue: ")
+    print("Welcome to ToBe. Enjoy your stay.")
+    print("\n")
 
-    a = GameState()
-    print(a.return_age())
-    print(a.is_game_over())
+    player = Player()
+    game = GameState()
+
+    while not game.game_over:
+
+        print("You are " + player.return_age() + " years old.")
+
+        game.play_turn(player)
+
+        if player.age == 5:
+
+            game.game_over = True
+        
+        input("Press ENTER to continue: ")
+        print()
+
+    print("You have died! You were " +
+          player.return_age() + " years old.")
 
 class GameState:
     
@@ -18,14 +33,32 @@ class GameState:
     and end game"""
 
     def __init__(self):
-        self.age = 0
+
         self.game_over = False
 
-    def return_age(self):
-        return self.age
+    def play_turn(self, player):
 
-    def is_game_over(self):
-        return self.game_over
+        player.add_year()
+
+class Player:
+
+    """Player class to store all the information on the
+    player character in the game"""
+
+    def __init__(self):
+
+        self.age = 0
+        self.country = "USA"
+        self.sex = "Male"
+        self.race = "White"
+
+    def add_year(self):
+
+        self.age += 1
+
+    def return_age(self):
+
+        return(str(self.age))
 
 if __name__ == "__main__":   
     main()
